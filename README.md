35168cb176aba2e420923eb74a28c86505bbb074
Niko42na <niko_42na@ukrbash.org>
2018-02-18 21:34:34 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# "NodeJS server"
+
+Little app for finding movies. 
+
+## Getting Started
+Just download/clone project, and use console command:
+`node server` - it will run server on port 3000. 
+Then go to `localhost:3000` and use app.
--- server.js
+++ server.js
@@ -1,60 +1,16 @@
 const http = require('http');
+const render = require('./lib/render');
+const { public, home, search, notFound } = require('./routes');
 
-const html = `
-        <!doctype>
-        <html>
-            <head>
-                <meta charset="utf-8">
-                <title>Node.js server tutorial</title>
-                <link rel="stylesheet" href="app.css"
-                <body>
-                    <h1>Node.js Essentials</h1>
-                    <button>Click me!</button>
-                    <script src="app.js"></script>
-                </body>
-            </head>
-        </html>
-`;
-const css = `
-    body {
-        margin: 0;
-        padding: 0;
-        text-align: center;
+
+http.createServer((req, res) => {
+    if (req.url.match(/.(html|css|js|png)$/)) {
+        public(req, res);
+    } else if (req.url === '/') {
+        home(req, res);
+    } else if (req.url.startsWith('/search')) {
+        search(req, res);
+    } else {
+        notFound(req, res);
     }
-    h1 {
-        background-color: #43853d;
-        color: white;
-        padding: .5em;
-        font-family: 'Consolas'
-    }
-`;
-
-const js = `
-    const button = document.querySelector('button');
-    button.addEventListener('click', event => alert('Node.js in action.'));
-`;
-
-http.createServer((req, res) =>{
-    switch(req.url){
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(html);
-            break;
-
-        case '/app.css':
-            res.writeHead(200, {'Content-Type': 'text/css'});
-            res.end(css);
-            break;
-
-        case '/app.js':
-            res.writeHead(200, {'Content-Type': 'text/js'});
-            res.end(js);
-            break;
-
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('404 not found');
-    }
-    res.writeHead(200, {'Content-Type': 'text/html' });
-    res.end(html);
-}).listen(5000, () => console.log('Server started -->'));
+}).listen(3000, () => console.log('Server started'));
